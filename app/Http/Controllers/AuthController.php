90b388ee7d5ddb8ca1acd94ee2de19a2e67d4db5
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-12-17 10:42:56 +0530

    AuthController.php Added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+class AuthController extends Controller
+{
+    public function register()
+    {
+        echo("Company registered successfully!");
+    }
+
+    public function login()
+    {
+        echo("Company login successfully!");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
-Route::get('save', [EmployeeController::class, 'save']);
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
