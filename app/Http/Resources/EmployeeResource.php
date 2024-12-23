24259c8ae13826a00e869767f0955f7cb7ae879f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-12-23 14:11:17 +0530

    Id Added to Employees

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -10,6 +10,7 @@ class EmployeeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'address' => $this->address,
             'salary' => $this->salary,
